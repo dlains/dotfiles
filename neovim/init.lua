82c7db64512bb31f10ae473852ae7b4cbcf57169
David Lains <dlains@gmail.com>
2026-09-13 14:38:21 -0400

    Fucking hell this should be easier.

--- neovim/init.lua
+++ neovim/init.lua
@@ -1,2 +1,26 @@
-require("dave")
-print("hello")
+-- Basic settings
+vim.o.number = true         -- Enable line numbers
+vim.o.relativenumber = true -- Enable relative line numbers
+vim.o.tabstop = 4           -- Number of spaces a tab represents
+vim.o.shiftwidth = 4        -- Number of spaces for each indentation
+vim.o.expandtab = true      -- Convert tabs to spaces
+vim.o.smartindent = true    -- Automatically indent new lines
+vim.o.wrap = false          -- Disable line wrapping
+vim.o.cursorline = true     -- Highlight the current line
+vim.o.termguicolors = true  -- Enable 24-bit RGB colors
+
+-- Syntax highlighting and filetype plugins
+vim.cmd('syntax enable')
+vim.cmd('filetype plugin indent on')
+
+vim.g.mapleader = ' '       -- Space as the leader key
+
+-- Additional configurations divided by type.
+require("dave.options")
+require("dave.keymaps")
+
+-- Plugins
+--require('dave.plugins')
+
+-- Configure UI components last
+require('dave.theme')
