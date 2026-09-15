0aa2c7708340ecb0ca6680175a0f2a0386e09336
David Lains <dlains@gmail.com>
2026-09-15 07:33:15 -0400

    Updated treesitter.

--- neovim/lua/treesitter.lua
+++ neovim/lua/treesitter.lua
@@ -16,3 +16,23 @@ require('nvim-treesitter').install {
     "markdown",
     "bash",
 }
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "*"
+    callback = function(args)
+        local buf = args.buf
+        local ft = vim.bo[buf].filetype
+
+        local lang = vim.treesitter.language.get_lang(ft)
+        if not lang then
+            return
+        end
+
+        local ok_add = pcall(vim.treesitter.language.add, lang)
+        if not ok_add then
+            return
+        end
+
+        pcall(vim.treesitter.start, buf, lang)
+    end,
+})
