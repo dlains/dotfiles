c8cb9c387ce5793116a209ea98f43dff8a1c50ca
David Lains <dlains@gmail.com>
2026-09-16 14:13:30 -0400

    Convert to single file Neovim setup

--- neovim/init.lua
+++ neovim/init.lua
@@ -1,14 +1,61 @@
-require("vim._core.ui2").enable({})
+-- Basic settings
+-- vim.g.netrw_banner = 0      -- Disable file browser banner
+vim.o.number = true         -- Enable line numbers
+vim.o.relativenumber = true -- Enable relative line numbers
+vim.o.tabstop = 4           -- Number of spaces a tab represents
+vim.o.softtabstop = 4       -- Number of spaces for a soft tab
+vim.o.shiftwidth = 4        -- Number of spaces for each indentation
+vim.o.expandtab = true      -- Convert tabs to spaces
+vim.o.smartindent = true    -- Automatically indent new lines
+vim.o.wrap = false          -- Disable line wrapping
+vim.o.cursorline = true     -- Highlight the current line
+vim.o.termguicolors = true  -- Enable 24-bit RGB colors
+vim.o.inccommand = "split"  --
+vim.o.splitbelow = true     -- Split help window below
+vim.o.splitright = true     -- Split help to the right
+vim.o.autocomplete = true   -- Turn on language autocomplete
+vim.o.signcolumn = "yes"    -- Allow space for messages next to numbers
+vim.o.winborder = "rounded" -- Show a border around dialog windows
 
--- Options
-require("options")
-require("keymaps")
+-- Keymaps
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
+vim.keymap.set('n', '<leader>w', ':write<CR>')
 
 -- Plugins
-require('plugins')
+vim.pack.add({
+    { src = "https://github.com/folke/tokyonight.nvim.git" },
+    { src = "https://github.com/echasnovski/mini.pick" },
+    { src = "https://github.com/neovim/nvim-lspconfig" }
+})
 
--- Set up plugins with dependencies
-require('treesitter') -- Set up before LSP for better highlighting
-require('lsp')
+require('mini.pick').setup()
 
-vim.cmd[[colorscheme tokyonight]]
+vim.keymap.set('n', '<leader>f', ':Pick files<CR>')
+
+-- Language Server Protocol
+vim.lsp.enable({
+    "lua_ls",
+    "clangd"
+})
+
+vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
+
+vim.lsp.config('clangd', {
+    cmd = { 'clangd' },
+    filetypes = { 'c', 'h' },
+})
+
+vim.lsp.config("lua_ls", {
+    settings = {
+        Lua = {
+            diagnostics = { globals = { "vim" } },
+        },
+    },
+})
+
+vim.keymap.set('n', 'gl', vim.diagnostic.open_float)
+
+vim.cmd [[colorscheme tokyonight]]
