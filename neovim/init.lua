37dea3019cb26dd9d9c9121454aa9ac1ff08e0bf
David Lains <dlains@gmail.com>
2026-09-24 08:16:17 -0400

    Updates to Neovim config.

--- neovim/init.lua
+++ neovim/init.lua
@@ -2,20 +2,51 @@
 -- vim.g.netrw_banner = 0      -- Disable file browser banner
 vim.o.number = true         -- Enable line numbers
 vim.o.relativenumber = true -- Enable relative line numbers
+vim.o.wrap = false          -- Disable line wrapping
+vim.o.cursorline = true     -- Highlight the current line
+vim.o.scrolloff = 10        --
+vim.o.sidescrolloff = 8     --
+
+-- Indentation
 vim.o.tabstop = 4           -- Number of spaces a tab represents
 vim.o.softtabstop = 4       -- Number of spaces for a soft tab
 vim.o.shiftwidth = 4        -- Number of spaces for each indentation
 vim.o.expandtab = true      -- Convert tabs to spaces
 vim.o.smartindent = true    -- Automatically indent new lines
-vim.o.wrap = false          -- Disable line wrapping
-vim.o.cursorline = true     -- Highlight the current line
+vim.o.autoindent = true     -- Copy indent from current line
+
+-- Search settings
+vim.o.ignorecase = true     -- Ignore case in searches
+vim.o.smartcase = true      -- Case sensitive if uppercase in search
+vim.o.hlsearch = false      -- Don't highlight search results
+vim.o.incsearch = true      -- Show matches as you type
+
+-- Visual settings
 vim.o.termguicolors = true  -- Enable 24-bit RGB colors
+vim.o.signcolumn = "yes"    -- Allow space for messages next to numbers
+vim.o.colorcolumn = "100"   -- Show column at 100 characters
+vim.o.showmatch = true      -- Highlight matching brackets
+vim.o.matchtime = 2         -- How long to show matching bracket
+vim.o.completeopt = "menuone,noinsert,noselect" -- ?
+vim.o.pumheight = 10        -- Popup menu height
+vim.o.winblend = 0          -- Floating window transparency
 vim.o.inccommand = "split"  --
 vim.o.splitbelow = true     -- Split help window below
 vim.o.splitright = true     -- Split help to the right
 vim.o.autocomplete = true   -- Turn on language autocomplete
-vim.o.signcolumn = "yes"    -- Allow space for messages next to numbers
 vim.o.winborder = "rounded" -- Show a border around dialog windows
+
+-- File handling settings
+vim.o.backup = false        -- Don't create backup files
+vim.o.writebackup = false   -- Don't create backup files before writing
+vim.o.swapfile = false      -- Don't create swap files
+vim.o.undofile = true       -- Persistent undo
+vim.o.undodir = vim.fn.expand("~/.vim/undodir") -- Undo directory
+vim.o.updatetime = 300      -- Faster completion
+vim.o.timeoutlen = 500      -- Key timeout duration
+vim.o.ttimeoutlen = 0       -- Key code timeout
+vim.o.autoread = true       -- Auto reload files changed outside of Neovim
+vim.o.autowrite = false     -- Don't auto save
 
 -- Keymaps
 vim.g.mapleader = " "
@@ -66,4 +97,4 @@ require("lualine").setup {
     }
 }
 
-vim.cmd [[colorscheme tokyonight]]
+vim.cmd.colorscheme("tokyonight")
